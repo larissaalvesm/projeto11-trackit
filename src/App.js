7a98fcc685f9a1494c3f90444f2238418069c7f4
larissa <larissaalves1102@gmail.com>
2023-03-14 14:25:22 -0300

    componentização das páginas

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import CadastroPage from "./pages/CadastroPage";
+import HabitosPage from "./pages/HabitosPage";
+import HistoricoPage from "./pages/HistoricoPage";
+import HojePage from "./pages/HojePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <>
-      oi
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}></Route>
+          <Route path="/cadastro" element={<CadastroPage />}></Route>
+          <Route path="/habitos" element={<HabitosPage />}></Route>
+          <Route path="/hoje" element={<HojePage />}></Route>
+          <Route path="/historico" element={<HistoricoPage />}></Route>
+
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
